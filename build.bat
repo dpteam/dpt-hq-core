70140ce2f0ffa0aa48dc4b277e8b6109064064fd
DartPower <SIMPSON-NEKEK@mail.ru>
2017-12-06 01:43:20 +0300

    GS[global-simple] refactoring & fixes, readme overhaul

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
 cd pk3_data
-..\7za a -t7z -m0=lzma2 -mx=9 -aoa ..\DPT-HQ-%date:~6,4%%date:~3,2%%date:~0,2%-%time:~0,2%%time:~3,2%.pk7 *
+..\7za a -t7z -m0=lzma2 -mx=9 -aoa "..\DPT-HQ-%date:~6,4%%date:~3,2%%date:~0,2%-%time:~0,2%%time:~3,2%.pk7" *
