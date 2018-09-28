7a31bdeb2aba018b47472de6f5bb4708d94ca134
DartPower <SIMPSON-NEKEK@mail.ru>
2018-09-28 17:13:48 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set +v
 echo "Compressing. Please wait... [Approximately 2-5 mins]"
 cd pk3_data
-..\7z a -t7z -m0=lzma2 -mx=9 -aoa "..\DPT-HQ-Core-current.pk7" *
+..\7z a -t7z -m0=lzma2 -mx=9 -aoa "..\dpt-hq-core-current.pk7" *
 clear
 echo "Compression complete."
 sleep
