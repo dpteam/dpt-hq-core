b2481fd444c0473e057e49b91e0177a13e3930cc
DartPower <dartpower@mail.ru>
2019-01-14 20:54:54 +0300

    Boss music overhaul

--- pk3_data/ACS_Source/hq_boss.c
+++ pk3_data/ACS_Source/hq_boss.c
@@ -1,9 +1,11 @@
 #include "zcommon.acs"
 #library "hq_boss"
 
+// Boss #1 Music change events
+
 script 1337 (void) NET
 {
-SetMusic("D_APS",0);
+SetMusic("D_BOSS1",0);
 }
 
 script 1338 (void) NET
@@ -11,9 +13,11 @@ script 1338 (void) NET
 SetMusic("*",0);
 }
 
+// Boss #3 Music change events
+
 script 1228 (void) NET
 {
-SetMusic("D_PAT",0);
+SetMusic("D_BOSS3",0);
 }
 
 script 1229 (void) NET
