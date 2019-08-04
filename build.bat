7305aa1522b570b22125ba1694dc93e4a1ac891b
DartPower <SIMPSON-NEKEK@mail.ru>
2019-08-04 16:57:21 +0300

    Simple fixes

--- build.bat
+++ build.bat
@@ -1,7 +1,8 @@
 @echo off
 SetLocal EnableExtensions EnableDelayedExpansion
 color 0B
-title Compressing. Please wait... [Approximately 2-5 mins]
+title Compressing, please wait... [Approximately 2-5 mins]
+echo Building DPT.HQ...
 cd pk3_data
 ..\7z a -t7z -m0=lzma2 -mx=9 -aoa "..\DPT-HQ-Core-%date:~6,4%%date:~3,2%%date:~0,2%-%time:~0,2%%time:~3,2%.pk7" *
 cls
