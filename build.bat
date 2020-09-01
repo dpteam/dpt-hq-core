bd2b728fecd30d96f4c01033561ba33c3a2fb1af
DartPower <SIMPSON-NEKEK@mail.ru>
2020-09-01 20:45:48 +0300

    Микрофикс батника

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@
 SetLocal EnableExtensions EnableDelayedExpansion
 color 0B
 title Compressing, please wait... [Approximately 2-5 mins]
-echo Building DPT.HQ...
+echo Building DPT.HQ Core component...
 cd pk3_data
 ..\7z a -t7z -m0=lzma2 -mx=9 -aoa "..\DPT-HQ-Core-%date:~6,4%%date:~3,2%%date:~0,2%-%time:~0,2%%time:~3,2%.pk7" *
 cls
